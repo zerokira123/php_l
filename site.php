93373417add6221a056f53b1aff4d409d3274d19
zerokira123 <thfication01@gmail.com>
2021-01-21 22:35:06 +0530

    first commit 00:30
    leanred about echo , html tags in php and php server

---
+++ site.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>php</title>
+</head>
+
+<body>
+    <?php
+        echo "<h1><em>Hello world</em></h1>";
+        echo "<hr> <br>";
+        echo "This is my site";
+    ?>
+</body>
+</html>
