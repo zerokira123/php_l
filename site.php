8638f3d3fc9ddeb68719c0674483694345316928
zerokira123 <thfication01@gmail.com>
2021-01-22 02:33:42 +0530

    55:00
    learned about string functions

--- site.php
+++ site.php
@@ -9,12 +9,25 @@
 
 <body>
     <?php
-        $pharse = "tobi or not to be";
+        $pharse = "Tobi or not to be";
+                   //0123456789.......
         $age = 30;
         $gpa = 30.3;
         $ismale = false;
         //null; these are the data types in php
         echo "<em><strong> $pharse </strong></em>";
+        echo "<br> <hr>";
+        echo strtolower($pharse);
+        echo "<br>";
+        echo strtoupper($pharse);
+        echo "<br>";
+        echo strlen($pharse);
+        echo "<br>";
+        echo $pharse[3];// tells char in that index
+        echo "<br>";
+        echo str_replace("Tobi", "obito", $pharse);
+        echo "<br>";
+        echo substr($pharse, 8, 3);
     ?>
 </body>
 </html>
