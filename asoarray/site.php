4d1227a2ed9b9533db0ede2120c568f3b835a353
zerokira123 <thfication01@gmail.com>
2021-02-05 01:01:32 +0530

    associate array
    learned about key map

---
+++ asoarray/site.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>php</title>
+</head>
+
+<body>
+    <?php
+    //error_reporting(E_ERROR | E_PARSE);
+    //error_reporting(E_ALL ^ E_WARNING); 
+        $grades =array("jim" => "A+","Pam"=>"B-","Oscar"=>"C+");
+        $grades["jim"] = "F";
+        echo $grades["jim"];
+    ?>
+</body>
+</html>
