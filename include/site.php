2103527580a25aaabf6f6c16205bf33fd3b11317
zerokira123 <thfication01@gmail.com>
2021-02-06 17:01:43 +0530

    3:36:00
    learned about how to include html files in php

---
+++ include/site.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>php</title>
+</head>
+
+<body>
+    <?php
+    //error_reporting(E_ERROR | E_PARSE);
+    //error_reporting(E_ALL ^ E_WARNING); 
+    //comments
+    include"header.html";
+
+    echo "<p>HELLO WORLD</p>";
+
+    include"footer.html";
+    ?>
+</body>
+</html>
